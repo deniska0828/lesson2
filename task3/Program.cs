ff1b93462f28de0779f00af6d3dd33a5b544d8e3
Denis <gubanov.denis.1996@gmail.com>
2024-02-07 01:03:00 +0800

    add lesson2:task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 5, 4, 6, 7, 8, 12, 45, 4, 12 };
+int i =0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
